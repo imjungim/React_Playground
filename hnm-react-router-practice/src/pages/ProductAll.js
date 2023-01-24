a1a32a1ca010766107d314a60bcfc3fa267216a0
imjungim <jungim4201@gmail.com>
2023-01-24 19:55:36 +0900

    my-json-server url 변경

--- hnm-react-router-practice/src/pages/ProductAll.js
+++ hnm-react-router-practice/src/pages/ProductAll.js
@@ -12,7 +12,7 @@ const ProductAll = () => {
   const getProducts = async () => {
     let searchQuery = query.get("q") || "";
     console.log("query?", searchQuery)
-    let url = `http://localhost:5000/products?q=${searchQuery}`
+    let url = `https://my-json-server.typicode.com/imjungim/react/products?q=${searchQuery}`
     let response = await fetch(url)
     let data = await response.json()
     setProductList(data)
--- hnm-react-router-practice/src/pages/ProductDetail.js
+++ hnm-react-router-practice/src/pages/ProductDetail.js
@@ -10,7 +10,7 @@ const ProductDetail = () => {
   let { id } = useParams()
   const [product, setProduct] = useState(null)
   const getProductDetail = async () => {
-    let url = `http://localhost:5000/products/${id}`
+    let url = `https://my-json-server.typicode.com/imjungim/react/products/${id}`
     let response = await fetch(url)
     let data = await response.json()
     setProduct(data)
