7c9fc452030367e1ad81282bea9fd7a4c92d4a5b
imjungim <jungim4201@gmail.com>
2023-01-24 16:50:35 +0900

    💄 productDetail page

--- hnm-react-router-practice/src/pages/ProductDetail.js
+++ hnm-react-router-practice/src/pages/ProductDetail.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/esm/Row'
+import Col from 'react-bootstrap/esm/Col'
+import Dropdown from 'react-bootstrap/Dropdown'
+import Button from 'react-bootstrap/Button'
 
 const ProductDetail = () => {
+  let { id } = useParams()
+  const [product, setProduct] = useState(null)
+  const getProductDetail = async () => {
+    let url = `http://localhost:5000/products/${id}`
+    let response = await fetch(url)
+    let data = await response.json()
+    setProduct(data)
+  }
+  useEffect(() => {
+    getProductDetail()
+  }, [])
+
   return (
-    <div>상품상세페이지</div>
+    <Container>
+      <Row>
+        <Col className="product-img">
+          <img src={product?.img} alt="product-image" />
+        </Col>
+        <Col>
+          <h4>{product?.title}</h4>
+          <h4>{product?.price.toLocaleString()}</h4>
+          <p>{product?.choice === true ? 'Conscious choice' : ''}</p>
+
+          <Dropdown>
+            <Dropdown.Toggle variant="black" id="dropdown-basic">
+              사이즈 선택
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">S</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">M</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">L</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <Button variant="dark" className="detail-btn">
+            추가
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
