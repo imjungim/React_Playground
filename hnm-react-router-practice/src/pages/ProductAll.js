a6a4bd2967dc0056275dc31967d3f7238b62872c
imjungim <jungim4201@gmail.com>
2023-01-24 18:38:04 +0900

    🚧 useSearchParams url 쿼리 가져오기

--- hnm-react-router-practice/src/pages/ProductAll.js
+++ hnm-react-router-practice/src/pages/ProductAll.js
@@ -3,21 +3,24 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import ProductCard from '../components/ProductCard';
-
+import { useSearchParams } from 'react-router-dom';
 
 const ProductAll = () => {
-  const [productList, setProductList] = useState([]);
+  const [productList, setProductList] = useState([])
+  const [query, setQuery] = useSearchParams();
 
   const getProducts = async () => {
-    let url = 'http://localhost:5000/products'
-    let response = await fetch(url);
-    let data = await response.json();
+    let searchQuery = query.get("q") || "";
+    console.log("query?", searchQuery)
+    let url = `http://localhost:5000/products?q=${searchQuery}`
+    let response = await fetch(url)
+    let data = await response.json()
     setProductList(data)
   }
-  
+
   useEffect(() => {
     getProducts()
-  },[])
+  }, [query]) //query가 바뀔때마다 실행
 
   return (
     <div>
