f32fc10a90ee871e2bcf8eeaf096fb5bd9b2db1a
imjungim <jungim4201@gmail.com>
2023-01-25 13:41:55 +0900

    useSelector, payload

--- redux-practice/src/App.js
+++ redux-practice/src/App.js
@@ -2,18 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Box from './component/Box';
 
 function App() {
-  const count = useSelector(state=>state.count) //store에 있는 모든 state를 매개 변수로 받는다.(그 전체 중 count의 값만)
+  const count = useSelector(state => state.count) //store에 있는 모든 state를 매개 변수로 받는다.(그 전체 중 count의 값만)
+  let id = useSelector(state => state.id)
+  let password = useSelector(state => state.password)
   const dispatch = useDispatch()
   const increase = () => {
-    dispatch({type : "INCREMENT"}) //action객체 {type: 액션이름, payload(옵션)} -> reducer로 이동 자동으로 dispatch가 던진 액션을 받아올 수 있다!!
+    dispatch({ type: "INCREMENT", payload: { num: 5 } }) //action객체 {type: 액션이름, payload(옵션)} -> reducer로 이동 자동으로 dispatch가 던진 액션을 받아올 수 있다!!
+    //payload 필요한 값을 보내줄 수 있다. 5씩 증가 ( 매개변수처럼 사용한다.)
+  }
+
+  const decrease = () => {
+    dispatch({ type: "DECREASE", payload: {num:2} })
+  }
+
+  const login = () => {
+    dispatch({type : "LOGIN", payload:{id:'jungim', password : "123"}})
   }
 
   return (
     <div className="App">
+      <h1>{id}, {password }</h1>
       <h1>{count}</h1>
       <button onClick={increase}>증가</button>
+      <button onClick={decrease }>감소</button>
+      <button onClick={login }>Login</button>
+      <Box/>
     </div>
   );
 }
--- redux-practice/src/redux/reducer/reducer.js
+++ redux-practice/src/redux/reducer/reducer.js
@@ -1,17 +1,29 @@
 //state 초기화 어떤 state가 있는지
 let initialState = {
-  count:0,
+  count: 0,
+  id: "",
+  password:"",
 }
 
 
 function reducer(state=initialState, action) {
-  console.log(action)//component에서  dispatch로 받아온 action
+ // console.log(action)//component에서  dispatch로 받아온 action
 
-  if (action.type === "INCREMENT") {
-    return {...state, count: state.count +1} //return 필수 state를 변경해줌
+  // if (action.type === "INCREMENT") {
+  //   return {...state, count: state.count + action.payload.num} //return 필수 state를 변경해줌
+  // }
+  // return {...state} //store는 return이 무조건 있어야 한다. 그래서 reducer에서 무조건 기본 리턴이 필요!
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, count: state.count + action.payload.num }
+    case "DECREASE":
+      return { ...state, count: state.count - action.payload.num}
+    case "LOGIN":
+      return { ...state, id: action.payload.id, password: action.payload.password}
+    default:
+      return {...state}
   }
 
-  return {...state} //store는 return이 무조건 있어야 한다. 그래서 reducer에서 무조건 기본 리턴이 필요!
 }
 
 export default reducer //store에 적용시켜준다.
