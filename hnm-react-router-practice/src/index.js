7e3e427fc580f7d2c1f05deb6681ca5f14bece08
imjungim <jungim4201@gmail.com>
2023-01-26 15:05:21 +0900

    🐛 BrowserRouter

--- hnm-react-router-practice/src/index.js
+++ hnm-react-router-practice/src/index.js
@@ -10,9 +10,11 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
