639124a5b4cc87dace73239a74e2ad911bf02f65
imjungim <jungim4201@gmail.com>
2023-01-27 14:12:05 +0900

    ⚡️ redux Devtool 적용

--- hnm-react-router-practice/src/redux/store.js
+++ hnm-react-router-practice/src/redux/store.js
@@ -1,7 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/index' 
 
-let store = createStore(rootReducer, applyMiddleware(thunk))
+let store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
 //reducer파일이 여러개(기능별)이면 합쳐서 적용을 해줘야한다.
 export default store                                                                                                       
