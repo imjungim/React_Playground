eb9ead282784f69ead5598dca7f913f106b09ad2
imjungim <jungim4201@gmail.com>
2023-06-01 21:11:29 +0900

    redux-toolkit_import createSlice

--- hnm-react-router-practice/src/redux/reducers/productReducer.js
+++ hnm-react-router-practice/src/redux/reducers/productReducer.js
@@ -1,15 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit'
+
 let initialState = {
   productList: [],
 }
 
-function productReducer(state = initialState, action) {
-  let { type, payload } = action
-  switch (type) {
-    case 'GET_PRODUCT_SUCCESS':
-      return { ...state, productList: payload.data }
-    default:
-      return {...state}
-  }
-}
+// function productReducer(state = initialState, action) {
+//   let { type, payload } = action
+//   switch (type) {
+//     case 'GET_PRODUCT_SUCCESS':
+//       return { ...state, productList: payload.data }
+//     default:
+//       return {...state}
+//   }
+// }
 
-export default productReducer
+//export default productReducer
+
+//createSlice가 반환하는값을 저장
+const productSlice = createSlice({
+  name:'product',
+  initialState,
+  reducers: {
+    getAllProducts(state, action) {
+      state.productList = action.payload.data //...state를 해주지 않아도 됨.
+    }
+  }
+})
+
+console.log("productSlice", productSlice)
+
+export default productSlice.reducer //하나의 큰 리듀서 export
+
